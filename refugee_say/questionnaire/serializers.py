e0bf5f680236e24da35473098daa1bab11c7111d
Abdulrahman Alotaibi <adminq80@gmail.com>
2017-08-10 20:03:44 -0400

    DIY serializer for Quetionnaire objects

--- refugee_say/questionnaire/serializers.py
+++ refugee_say/questionnaire/serializers.py
@@ -1,20 +1,54 @@
-from rest_framework import serializers
 from django.db import IntegrityError
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 
 from .models import QuestionOrder, Questionnaire
-
 from refugee_say.radio_question.models import RadioQuestion
 from refugee_say.ranking_question.models import RankingQuestion
 from refugee_say.selection_question.models import SelectionQuestion
 
 
-class QuestionnaireSerializer(serializers.ModelSerializer):
+class QuestionRelatedField(serializers.RelatedField):
+
+    def to_representation(self, value):
+        print(type(value))
+        if isinstance(value, RadioQuestion):
+            return super().to_representation(value)
+        if isinstance(value, RankingQuestion):
+            return super().to_representation(value)
+        if isinstance(value, SelectionQuestion):
+            return super().to_representation(value)
+        return super().to_representation(value)
+        # raise Exception(_('Unexpected type of tagged object'))
+
+    def to_internal_value(self, data):
+        super().to_internal_value(data)
+
+
+class QuestionnaireSerializer_(serializers.ModelSerializer):
+    questions = serializers.PrimaryKeyRelatedField(queryset=Questionnaire.objects.values_list('radios', 'ranks', 'selections'))
+
+    def get_questions(self, obj: Questionnaire):
+        questions = QuestionOrder.objects.filter(questionnaire=obj).order_by('order')
+        print(questions)
+        # questions = []
+        # for item in obj.radios:
+        #     questions.append(item)
+        # for item in obj.ranks:
+        #     questions.append(item)
+        # for item in obj.selections:
+        #     questions.append(item)
+        return questions
 
     class Meta:
-        depth = 1
         model = Questionnaire
-        fields = ('id', 'description', 'created_at', 'updated_at', 'language', 'radios', 'ranks', 'selections')
+        fields = ('id', 'description', 'created_at', 'updated_at', 'language', 'questions', )
+        read_only_fields = ('questions', )
+        depth = 1
+
 
 class QuestionOrderSerializer(serializers.ModelSerializer):
     radio = serializers.PrimaryKeyRelatedField(allow_null=True, queryset=RadioQuestion.objects.all())
@@ -38,12 +72,47 @@ class QuestionOrderSerializer(serializers.ModelSerializer):
         s -= {None}
         s = list(s)
         if len(s) == 1 and isinstance(s[0], (RadioQuestion, RankingQuestion, SelectionQuestion)):
-            if s[0].language != attrs.get('questionnaire').language:
-                raise  serializers.ValidationError(_("Languages didn't match"))
-            return super().validate(attrs)
+            if s[0].language == attrs.get('questionnaire').language:
+                return super().validate(attrs)
+            raise  serializers.ValidationError(_("Languages didn't match"))
         raise serializers.ValidationError(_('Order needs to be assigned to only one of the questions'))
 
     class Meta:
         depth = 1
         model = QuestionOrder
         fields = ('order', 'rank', 'radio', 'selection', 'questionnaire')
+
+
+
+class QuestionnaireSerializerV2(serializers.ModelSerializer):
+
+    questions = QuestionOrderSerializer(source='questionorder_set', many=True)
+
+    class Meta:
+        model = Questionnaire
+        fields = ('id', 'description', 'created_at', 'updated_at', 'language', 'questions', 'questions')
+
+
+class QuestionnaireSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+    language = serializers.ChoiceField(default=settings.LANGUAGE_CODE, choices=settings.LANGUAGES)
+    description = serializers.CharField(allow_blank=True)
+    # questions = serializers.ListField(allow_null=True, allow_empty=True)
+    questions = QuestionOrderSerializer(many=True, allow_null=True)
+        # serializers.PrimaryKeyRelatedField(allow_null=True, read_only=True)
+
+    def create(self, validated_data):
+        validated_data.pop('questions')
+        return Questionnaire.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        pass
+
+    def to_representation(self, instance):
+        print('To representation')
+        print(instance.questions)
+        print(JSONRenderer().render(QuestionOrderSerializer(instance.questions, many=True).data))
+        # instance.questions = JSONRenderer().render(QuestionOrderSerializer(instance.questions, many=True).data)
+        return super().to_representation(instance)
--- refugee_say/questionnaire/views.py
+++ refugee_say/questionnaire/views.py
@@ -1,23 +1,54 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, generics
+from rest_framework.response import Response
 
 from refugee_say.contrib.permissions import IsAdminOrReadOnly
 from .models import Questionnaire, QuestionOrder
 from .serializers import QuestionnaireSerializer, QuestionOrderSerializer
 
+from .serializers import QuestionnaireSerializerV2
 
-
-class QuestionnaireViewSet(viewsets.ModelViewSet):
-    model = Questionnaire
+class QuestionnaireViewSet__(viewsets.ReadOnlyModelViewSet):
     permission_classes = (IsAdminOrReadOnly, )
-    serializer_class =  QuestionnaireSerializer
+    serializer_class =  QuestionnaireSerializerV2
     queryset = Questionnaire.objects.all()
 
-    # def get_queryset(self):
-    #     return Questionnaire.objects.all()
+
+class QuestionnaireViewSet(viewsets.ViewSet):
+    queryset = Questionnaire.objects.all()
+    def get_queryset(self):
+        return self.queryset
+
+
+    def list(self, request):
+        qs = self.get_queryset()
+        l = set()
+        for q in qs:
+            q.questions = QuestionOrder.objects.filter(questionnaire=q).order_by('order')
+            l.add(q)
+        return Response(QuestionnaireSerializer(l, many=True).data)
+
+
+
+    # queryset = Questionnaire.objects.all()
+    # # serializer_class = QuestionnaireSerializer
+    #
+    # def get_serializer(self, *args, **kwargs):
+    #     print(args)
+    #     print(kwargs)
+    #     items = []
+    #     for questionnaire in args[0]:
+    #         questionnaire.questions = []
+    #             # JSONRenderer().render(QuestionOrderSerializer(
+    #             # QuestionOrder.objects.filter(questionnaire=questionnaire).order_by('order'), many=True).is_valid().data)
+    #         print(questionnaire.questions)
+    #         items.append(questionnaire)
+    #     return super().get_serializer(items, *args, **kwargs)
+    #
+    # def get_serializer_class(self):
+    #     return QuestionnaireSerializer
 
 
 class QuestionOrderViewSet(viewsets.ModelViewSet):
-    model = QuestionOrder
     permission_classes = (IsAdminOrReadOnly, )
     serializer_class = QuestionOrderSerializer
     queryset = QuestionOrder.objects.all()
